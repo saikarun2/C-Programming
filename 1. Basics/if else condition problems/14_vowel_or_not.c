310e3b99b3f466719d6e7e56741893143a97f1e9
saikarun2 <saikarunanaparthi@gmail.com>
2026-07-08 21:20:22 +0530

    add new problem

---
+++ 1. Basics/if else condition problems/14_vowel_or_not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter the character: ");
+    scanf("%c",&ch);
+    if(ch =='a'||ch =='e'||ch =='i'||ch =='o'||ch =='u'||ch =='A'||ch =='E'||ch =='I'||ch =='O'||ch =='U'){
+        printf("The given character is vowel");
+    }
+   else{
+        printf("The given character is consonants");
+    }
+}
