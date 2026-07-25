6242551d2a87d92a5f042233ff0cf87cb3b50ffb
saikarun2 <saikarunanaparthi@gmail.com>
2026-07-25 06:20:07 +0530

    new problem solved

---
+++ 1. Basics/if-else-if ladder/6_Bmi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    float height,weight,bmi;
+    printf("enter the heigh of the person(in meters): ");
+    scanf("%f",&height);
+    printf("enter the weight of the person(in kg): ");
+    scanf("%f",&weight);
+    bmi = weight/(height * height);
+    printf("\nYour BMI = %.2f\n", bmi);
+    if (bmi < 18.5){
+        printf("Category: Underweight\n");
+    }
+    else if (bmi >= 18.5 && bmi < 25){
+        printf("Category: Normal weight\n");
+    }
+    else if (bmi >= 25 && bmi < 30){
+        printf("Category: Overweight\n");
+    }
+    else{
+        printf("Category: Obese\n");
+    }
+    return 0;
+}
