e16f154f204cd71bc36a2025589c98f059a84fab
saikarun2 <saikarunanaparthi@gmail.com>
2026-07-25 06:49:07 +0530

    new problem solved

---
+++ 1. Basics/if-else-if ladder/10_discount.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int price, dis;
+    printf("ente the price value: ");
+    scanf("%d",&price);
+    if(price<1000){
+        dis = (5 * price)/100;
+    }
+    else if(price>=1000 && price<=5000){
+        dis = (10 * price)/100;
+    }
+    else{
+        dis = (15 * price)/100;
+    }
+    printf("the final price after the discount= %d",price-dis);
+    return 0;
+}
