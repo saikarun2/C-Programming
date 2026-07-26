2a2a7fc71e8802f298b7c5e64d7d34e91f585a61
saikarun2 <saikarunanaparthi@gmail.com>
2026-07-26 15:21:36 +0530

    new problem solved

--- 1. Basics/if-else-if ladder/14_grade_to_gpa.c
+++ 1. Basics/if-else-if ladder/14_grade_to_gpa.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-    
+    char grade;
+    printf("enter the grade(A/B/C/D/E/F): ");
+    scanf("%c",&grade);
+    if(grade =='A' || grade =='a'){
+        printf("Your GPA is 10\n");
+    }
+    else if(grade == 'B' || grade =='b'){
+        printf("Your GPA is 8\n");
+    }
+    else if(grade == 'C' || grade =='c'){
+        printf("Your GPA is 6\n");
+    }
+    else if(grade == 'D' || grade =='d'){
+        printf("Your GPA is 4\n");
+    }
+    else if(grade == 'F' || grade =='f'){
+        printf("Your GPA is 0\n");
+    }
+    else{
+        printf("Invalid Grade\n");
+    }
     return 0;
 }
