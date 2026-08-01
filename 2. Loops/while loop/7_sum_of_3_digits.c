ccea3fb8c78ffd6832b599d1fe110be950ab7d56
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-01 21:37:30 +0530

    sum of the digits using while loop

---
+++ 2. Loops/while loop/7_sum_of_3_digits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,sum=0,rem;
+    printf("enter the number(100 - 999): ");
+    scanf("%d",&num);
+    while (num>0)
+    {
+        rem =num%10;
+        sum += rem;
+        num/=10;
+    }
+    printf("the sume of the given number is %d",sum);
+    return 0;
+}
