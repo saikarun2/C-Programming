7b2e9737fa9d8fec1a2f78896c1f3e2af8725a5b
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-03 21:04:54 +0530

    decimal to binary problem solved using while loop

---
+++ 2. Loops/while loop/24_decimal_to_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n, binary[32], i = 0;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    if (n == 0) {
+        printf("Binary equivalent: 0\n");
+        return 0;
+    }
+    while (n > 0) {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    printf("Binary equivalent: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+    return 0;
+}
