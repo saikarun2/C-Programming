ba2edf6e5eb89097930c2a90b324347e70b53b2b
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-04 19:01:20 +0530

    print max value in array problem solved using for loop

---
+++ 2. Loops/2. for loop/6_max_value_in_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int max =0;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    printf("the max value in the array = %d",max);
+    
+    return 0;
+}
