41792ea200802405390a2b790ca8e22a2a7fc2b8
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-08 18:52:42 +0530

    sum of digits problem using do while loop

---
+++ 2. Loops/3. do while loop/4_sum_of_digits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num, digit, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    do {
+        digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    } while (num != 0);
+    printf("Sum of digits = %d\n", sum);
+    return 0;
+}
