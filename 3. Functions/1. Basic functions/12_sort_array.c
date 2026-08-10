142baba22974cdb37e7507b4ffc33a7dab5291cf
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-10 21:19:40 +0530

    sort an array using function

---
+++ 3. Functions/1. Basic functions/12_sort_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void sortArray(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int arr[] = {5, 2, 8, 1, 3};
+    int n = 5;
+    sortArray(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
