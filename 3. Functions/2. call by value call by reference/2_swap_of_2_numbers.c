1b286bd1e24354c3eea2264036b887a71da34521
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-11 21:11:49 +0530

    swap 2 numbers using call by reference using function

---
+++ 3. Functions/2. call by value call by reference/2_swap_of_2_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *a , int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    // printf("in swap function  after swap a = %d  and b = %d\n",a,b);
+}
+int main()
+{
+    int a = 5 ,b=10;
+    printf("in main  before function call a = %d  and b = %d\n",a,b);
+    swap(&a,&b);
+    printf("in main  after function call a = %d  and b = %d\n",a,b);
+    return 0;
+}
