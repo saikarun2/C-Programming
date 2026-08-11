4f8a2fe052fb8604084de0f2ce6c9c9d99c68635
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-11 21:15:47 +0530

    modify the array using function

---
+++ 3. Functions/2. call by value call by reference/3_modify_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void modifyElement(int *arr)
+{
+    arr[2] = 100;
+}
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    printf("The array before function\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    modifyElement(arr);
+     printf("The array after function\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
