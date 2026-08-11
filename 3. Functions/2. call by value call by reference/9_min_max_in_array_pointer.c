bf7284fe2f2f14346e507f75f235c5d59f9ea02c
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-11 21:29:53 +0530

    swap 2 numbers using xor pointer function

---
+++ 3. Functions/2. call by value call by reference/9_min_max_in_array_pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void findMinMax(int *arr, int n, int *min, int *max)
+{
+    int i;
+    *min = *arr;
+    *max = *arr;
+    for (i = 1; i < n; i++)
+    {
+        if (*(arr + i) < *min)
+            *min = *(arr + i);
+        if (*(arr + i) > *max)
+            *max = *(arr + i);
+    }
+}
+int main()
+{
+    int arr[] = {25, 10, 45, 5, 30};
+    int n = 5;
+    int min, max;
+    findMinMax(arr, n, &min, &max);
+    printf("Minimum = %d\n", min);
+    printf("Maximum = %d\n", max);
+    return 0;
+}
