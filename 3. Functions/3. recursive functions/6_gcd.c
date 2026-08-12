7ad412d5fa869a3ee0302d28b67c48ea1bbc5ede
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-12 20:04:01 +0530

    gcd using recursive function

---
+++ 3. Functions/3. recursive functions/6_gcd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int gcd(int a, int b)
+{
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+int main()
+{
+    int a = 48;
+    int b = 18;
+    printf("GCD = %d", gcd(a, b));
+    return 0;
+}
