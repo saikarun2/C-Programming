e6098849f9fba35b2b0ef6f3e555f627d3ca83f6
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-23 10:55:45 +0530

    boundarys of a matrix problem in 2d array

---
+++ 4. Array/3. 2d arrays/15_boundary_elements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    int matrix[rows][cols];
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("Boundary elements:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (i == 0 || i == rows - 1 ||
+                j == 0 || j == cols - 1)
+            {
+                printf("%d ", matrix[i][j]);
+            }
+        }
+    }
+    return 0;
+}
