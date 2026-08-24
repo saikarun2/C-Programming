2533a25f5489c1c2509a47165eb41458cd7809f0
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-24 21:22:21 +0530

    count no of vowles and consonants problem in string

---
+++ 4. Array/4. C Strings Basics/2_count_vowels_cons.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, i;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (isalpha(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowels++;
+            }
+            else{
+                consonants++;
+            }
+        }
+    }
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+    return 0;
+}
