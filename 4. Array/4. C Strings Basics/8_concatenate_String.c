953ac0bd1d463f1ec2384f7e341bf91ead20fd6e
saikarun2 <saikarunanaparthi@gmail.com>
2026-08-26 12:06:13 +0530

    concatenate two strings

---
+++ 4. Array/4. C Strings Basics/8_concatenate_String.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    char str1[200], str2[100];
+    int i = 0, j = 0;
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    while (str1[i] != '\0')
+    {
+        if (str1[i] == '\n')
+        {
+            str1[i] = '\0';
+            break;
+        }
+        i++;
+    }
+    while (str2[j] != '\0' && str2[j] != '\n')
+    {
+        str1[i++] = str2[j++];
+    }
+    str1[i] = '\0';
+    printf("Concatenated string: %s\n", str1);
+    return 0;
+}
