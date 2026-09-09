6fd07d66ad122eec8af68d58de96b2d143098e35
saikarun2 <saikarunanaparthi@gmail.com>
2026-09-09 21:32:26 +0530

    using arithmetic pointer like increment and decrement of array to ge the values

---
+++ 5. Pointers/1. Basics/6_increment_decrement_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+    printf("Current value: %d\n", *ptr);
+    ptr++;
+    printf("After increment: %d\n", *ptr);
+    ptr++;
+    printf("After increment: %d\n", *ptr);
+    ptr--;
+    printf("After decrement: %d\n", *ptr);
+    return 0;
+}
