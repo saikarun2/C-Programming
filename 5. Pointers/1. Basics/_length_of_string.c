62f9e1f28e90d7118c766abeb4bd4011075ed9ca
saikarun2 <saikarunanaparthi@gmail.com>
2026-09-15 19:46:24 +0530

    length of the string using pointer

---
+++ 5. Pointers/1. Basics/_length_of_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    char str[100];
+    char *p;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    p = str;
+    while (*p && *p != '\n'){
+        p++;
+    }
+    printf("Length = %ld\n", p - str);
+    return 0;
+}
